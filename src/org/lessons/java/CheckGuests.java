fd605958d707fdc3cf12488da030943dd3d6b4ed
LambertoNeri <neri.lamberto91@gmail.com>
2023-10-18 16:12:34 +0200

    changed name ferragnez party to checkguest, should be fixed but not optimized

---
+++ src/org/lessons/java/CheckGuests.java
@@ -0,0 +1,29 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class CheckGuests {
+    public static void main(String[] args) {
+        Scanner nameScan = new Scanner(System.in);
+        String guestName;
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+
+        System.out.println("Insert your name and surname");
+        guestName = nameScan.nextLine();
+        nameScan.close();
+        boolean isAuthorized = false;
+
+        for (int i = 0; i < guests.length; i++) {
+
+            if (guestName.equals(guests[i]) == true) {
+                isAuthorized = true;
+            }
+        }
+        if (isAuthorized){
+            System.out.println("Allowed");
+        } else {
+            System.out.println("not Allowed");
+        }
+    }
+}
